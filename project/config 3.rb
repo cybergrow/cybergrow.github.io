2de62d79c2ef550090ed77655e3f9708945afd61
Ted Gamester <tedgamester@me.com>
2017-04-28 11:08:55 -0700

    updated

---
+++ project/config 3.rb
@@ -0,0 +1,53 @@
+# use Compass for advanced CSS
+compass_config do |config|
+  config.output_style = :compact
+end
+
+helpers do
+  def nav_li(text, url)
+    trailurl = url
+    trailurl += '/' unless url == '/'
+    if trailurl == current_page.url
+      active = 'active'
+    end
+    content_tag :li, class: active do
+      link_to(text, url)
+    end
+  end
+end
+
+activate :directory_indexes
+
+activate :data_source do |c|
+  c.root  = "https://images.freifunk-duesseldorf.de/gluon/stable/"
+  c.files = [
+    "version.json"
+  ]
+end
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
+  activate :asset_hash
+end
+
+# deploy Netlify redirects file
+page 'redirects', :layout => false
+after_build do
+  File.rename 'build/redirects', 'build/_redirects'
+end
+
+require 'html-proofer'
+
+after_build do |builder|
+  begin
+    HTMLProofer.check_directory(config[:build_dir], {
+      :assume_extension => true,
+      :url_ignore => ["http://192.168.0.1", "http://192.168.1.1"]
+    }).run
+  rescue RuntimeError => e
+    puts e
+    exit 1
+  end
+end
---
+++ project/source/javascripts/all.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require bootstrap-sprockets
+//= require moment.js
+//= require fullcalendar.js
+//= require fullcalendar-de.js
+//= require gcal.js
+//= require google-tag-manager.js
+//= require application.js
